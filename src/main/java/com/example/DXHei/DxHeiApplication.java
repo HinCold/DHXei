5ab7eae8f3694428304c522695a407421065bbe9
XPR <984822045@qq.com>
2023-10-04 13:22:21 +0800

    create project DengXiaHei

---
+++ src/main/java/com/example/DXHei/DxHeiApplication.java
@@ -0,0 +1,16 @@
+package com.example.DXHei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+public class DxHeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxHeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DXHei/DxHeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DXHei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxHeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
